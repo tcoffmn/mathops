11a279df89ef59af40f4e8fcedb1d31f5b8c9756
Tony Coffman <tcoffmn@yahoo.com>
2021-06-11 21:18:46 -0500

    Add main function

--- mathops.py
+++ mathops.py
@@ -1,9 +1,16 @@
 import math
 import numbers
+import sys
 
-a = -90
+def print_sin():
+    a = -90
 
+    while a <= 90:
+        print(a, '=', math.sin(a))
+        a=a+1
 
-while a <= 90:
-    print(a, '=', math.sin(a))
-    a=a+1
+if __name__ == '__main__':
+
+    print_sin()
+
+    sys.exit(0)
